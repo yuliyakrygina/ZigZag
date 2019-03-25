dc37d88b6c9157ad72f206d04eb8c1f8a495bf25
Yuliya K <36555259+yuliyakrygina@users.noreply.github.com>
2019-03-25 14:57:43 -0400

    Adding script for movement

---
+++ Zig Zag Game/Assets/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+	Rigidbody rb;
+	public float speed; 
+
+	void Awake()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+		float inputX = Input.GetAxis("Horizonal");
+		float inputY = Input.GetAxis("Vertical");
+
+    }
+}
